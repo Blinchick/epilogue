33b62b63f17193f4f669cfc53dca933c67f206bc
Blinchick <annaspiridonenkova@gmail.com>
2020-06-10 02:47:04 -0400

    Updates

---
+++ precache-manifest.7467002c0ae9a2e7fd59b2ebf15c73ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c603c594a6fadebf55cfe19367b1044",
+    "url": "/epilogue/index.html"
+  },
+  {
+    "revision": "90536ea545af5d1fc59a",
+    "url": "/epilogue/static/css/main.0b5a6da0.chunk.css"
+  },
+  {
+    "revision": "2051b7e8ae609d1ed9ef",
+    "url": "/epilogue/static/js/2.fc2e6d94.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/epilogue/static/js/2.fc2e6d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90536ea545af5d1fc59a",
+    "url": "/epilogue/static/js/main.dfbfd3d3.chunk.js"
+  },
+  {
+    "revision": "5b32cecc719a91c5efdc",
+    "url": "/epilogue/static/js/runtime-main.37a0eeeb.js"
+  }
+]);
